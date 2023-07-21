cbf88455ea1fc97e8ec4339bab2904aa500d0ffc
Pavan <pavanraj97@gmail.com>
2023-07-21 17:41:26 -0300

    Operators

---
+++ 02-operators.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ARITHMETIC OPERATORS
+	var num1 = 4
+	var num2 = 3
+
+	var sum = num1 + num2
+	var difference = num1 - num1
+	var product = num1 * num2
+	var quotient = num1 / num2
+	var remainder = num1 % 2
+
+	// sum = sum + 1
+	// sum++
+	sum += 5
+	fmt.Println(sum, difference, product, quotient, remainder)
+
+	// RELATIONAL OPERATORS
+
+	var result = num1 > num2
+	result = num1 >= num2
+	result = num1 < num2
+	result = num1 <= num2
+	result = num1 == num2
+	result = num1 != num2
+
+	fmt.Println(result)
+
+	// LOGICAL OPERATORS
+
+	const name = "PavanRaj"
+	var age = 25
+
+	var inviteToParty = name == "Pavan" && age > 23
+	inviteToParty = name == "Pavan" || age >= 21
+	inviteToParty = name == "Pavan" || (age >= 21 && age < 90)
+	inviteToParty = !(name == "Pavan") && (age >= 21 && age < 90)
+
+	fmt.Println(inviteToParty)
+}
